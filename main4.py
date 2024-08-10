29e63dc364f5679c4546a37becdbec3da5d794e4
Raman <115335978+rmnjaat@users.noreply.github.com>
2024-08-10 17:05:50 +0530

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,17 @@
+from mindee import Client, PredictResponse, product
+
+# Init a new client
+mindee_client = Client(api_key="db6dce8aeb7de126d5cfe5a1054b00a8")
+
+# Load a file from disk
+input_doc = mindee_client.source_from_path("abc.pdf")
+
+# Load a file from disk and parse it.
+# The endpoint name must be specified since it cannot be determined from the class.
+result: PredictResponse = mindee_client.parse(product.InvoiceV4, input_doc)
+
+# Print a summary of the API result
+# print(result.document)
+
+# Print the document-level summary
+print(result.document.inference.prediction)
